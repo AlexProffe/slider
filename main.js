85b6647fe0a50871dfe6d4bbfceba5b26d098879
Alex <aliaksei.sholakhau@netcracker.com>
2022-08-10 23:53:27 +0300

    Added autoslide functionality

--- main.js
+++ main.js
@@ -1,111 +1,179 @@
 const defaultOptions = {
-    wrapperSelector: '.slider-wrapper',
-    itemSelector: '.slider-item',
-    prevArrow: 'prev',
-    nextArrow: 'next',
-    slidesPerSlide: 2,
-    slidesPerPage: 5,
+  wrapperSelector: ".slider-wrapper",
+  itemSelector: ".slider-item",
+  prevArrow: "prev",
+  nextArrow: "next",
+  slidesPerSlide: 2,
+  slidesPerPage: 5,
 };
 
 function slider(selector, options = {}) {
-    const mergedOptions = {...defaultOptions, ...options};
-    const { wrapperSelector, itemSelector, prevArrow, nextArrow, slidesPerPage, slidesPerSlide } = mergedOptions;
-    const prevArrowButton = document.getElementById(prevArrow);
-    const nextArrowButton = document.getElementById(nextArrow);
+  const mergedOptions = { ...defaultOptions, ...options };
+  const {
+    wrapperSelector,
+    itemSelector,
+    prevArrow,
+    nextArrow,
+    slidesPerPage,
+    slidesPerSlide,
+    timeout,
+    autoSlide,
+    loop,
+  } = mergedOptions;
+  const prevArrowButton = document.getElementById(prevArrow);
+  const nextArrowButton = document.getElementById(nextArrow);
 
-    const swipePositions = {};
+  const swipePositions = {};
 
-    const slider = document.getElementById(selector);
-  
+  const slider = document.getElementById(selector);
 
-    const wrapper = slider.querySelector(wrapperSelector);
-    const items =   wrapper.querySelectorAll(itemSelector);
-    const itemsCount = items.length;
+  const wrapper = slider.querySelector(wrapperSelector);
+  const items = wrapper.querySelectorAll(itemSelector);
+  const itemsCount = items.length;
 
-    let sliderWidth, itemWidth, position;
+  let sliderWidth, itemWidth, position, isEnd, isStart, intervalID, slideIndex = 0;
 
-    const caluclateSliderItemWidth = () => {
-        const previousWidth = itemWidth;
-       
-        sliderWidth = slider.offsetWidth;
-        itemWidth = sliderWidth / slidesPerPage;
-        items.forEach(item => item.style.width = `${itemWidth}px`);
-        const newPosition = isNaN(position / previousWidth) ? 0 : (position / previousWidth) * itemWidth;
-        position = newPosition;
-        wrapper.classList.add('no-anim');
-        moveWrapper();
-        setTimeout(() => {wrapper.classList.remove('no-anim')}, 0);
+  const caluclateSliderItemWidth = () => {
+    const previousWidth = itemWidth;
+
+    sliderWidth = slider.offsetWidth;
+    itemWidth = sliderWidth / slidesPerPage;
+    items.forEach((item) => (item.style.width = `${itemWidth}px`));
+    const newPosition = isNaN(position / previousWidth)
+      ? 0
+      : (position / previousWidth) * itemWidth;
+    position = newPosition;
+    wrapper.classList.add("no-anim");
+    moveWrapper();
+    setTimeout(() => {
+      wrapper.classList.remove("no-anim");
+    }, 0);
+  };
+
+  caluclateSliderItemWidth();
+
+  window.addEventListener("resize", caluclateSliderItemWidth);
+
+  prevArrowButton.addEventListener("click", () => {
+    if (autoSlide) {
+      clearInterval(intervalID);
+      triggerAutoSlide();
     }
+    calculateDescendingDirection();
+  });
 
-    caluclateSliderItemWidth();
+  function calculateDescendingDirection() {
+    slideIndex--;
+    console.log(slideIndex);
+    const leftItems = Math.abs(position / itemWidth);
 
-    window.addEventListener('resize', caluclateSliderItemWidth);
+    position +=
+      leftItems > slidesPerSlide
+        ? itemWidth * slidesPerSlide
+        : itemWidth * leftItems;
 
-    prevArrowButton.addEventListener('click', calculateDescendingDirection);
-
-    function calculateDescendingDirection() {
-        const leftItems = Math.abs(position / itemWidth);
-
-        position += leftItems > slidesPerSlide
-            ? itemWidth * slidesPerSlide
-            : itemWidth * leftItems;
-
-        moveWrapper();
-        rangeCheck();
-    };
-
-    nextArrowButton.addEventListener('click', calculateAscendingDirection);
-
-    function calculateAscendingDirection() {
-        const leftItems = itemsCount - (Math.abs(position / itemWidth) + slidesPerPage);
-        position -= leftItems > slidesPerSlide 
-            ? itemWidth * slidesPerSlide
-            : itemWidth * leftItems;
-        moveWrapper();
-        rangeCheck();
-    }
-
-    function moveWrapper() {
-        wrapper.style.transform = `translateX(${position}px)`;
-    }
-
-    function rangeCheck() {
-        position === 0
-            ? prevArrowButton.setAttribute('disabled', true)
-            : prevArrowButton.removeAttribute('disabled');
-
-        const rightEdgePosition = (itemsCount - slidesPerPage) * itemWidth;
-
-        Math.abs(position) === rightEdgePosition
-            ? nextArrowButton.setAttribute('disabled', true)
-            : nextArrowButton.removeAttribute('disabled');  
-    }
-
+    moveWrapper();
     rangeCheck();
+  }
 
-    wrapper.addEventListener('touchstart', event => {
-       swipePositions.startPosition = event.changedTouches[0].clientX;
-    }, {passive: true});
+  nextArrowButton.addEventListener("click", () => {
+    if (autoSlide) {
+      clearInterval(intervalID);
+      triggerAutoSlide();
+    }
+    calculateAscendingDirection();
+  });
 
-    wrapper.addEventListener('touchend', event => {
-        const endPosition = event.changedTouches[0].clientX;
-        const { startPosition } = swipePositions;
-        const difference = startPosition - endPosition;
-        if(Math.abs(difference) < 50) {
-            return;
-        }
-       
-        difference > 0 
-            ? calculateAscendingDirection()
-            : calculateDescendingDirection();
-    })
-}   
+  function calculateAscendingDirection() {
+    slideIndex++;
+    console.log(slideIndex);
+    const leftItems =
+      itemsCount - (Math.abs(position / itemWidth) + slidesPerPage);
+    position -=
+      leftItems > slidesPerSlide
+        ? itemWidth * slidesPerSlide
+        : itemWidth * leftItems;
+    moveWrapper();
+    rangeCheck();
+  }
 
-slider('slider', {
-    wrapperSelector: '.slider-wrapper',
-    itemSelector: '.slider-item',
-    prevArrow: 'prev',
-    nextArrow: 'next',
-    slidesPerSlide: 1,
-    slidesPerPage: 2,
+  function moveWrapper() {
+    wrapper.style.transform = `translateX(${position}px)`;
+  }
+
+  function rangeCheck() {
+    if (position === 0) {
+      prevArrowButton.setAttribute("disabled", true);
+      isStart = true;
+      isEnd = false;
+    } else {
+      prevArrowButton.removeAttribute("disabled");
+    }
+
+    const rightEdgePosition = (itemsCount - slidesPerPage) * itemWidth;
+    if (Math.abs(position) === rightEdgePosition) {
+      nextArrowButton.setAttribute("disabled", true);
+      isStart = false;
+      isEnd = true;
+    } else {
+      nextArrowButton.removeAttribute("disabled");
+    }
+  }
+
+  rangeCheck();
+
+  wrapper.addEventListener(
+    "touchstart",
+    (event) => {
+      swipePositions.startPosition = event.changedTouches[0].clientX;
+    },
+    { passive: true }
+  );
+
+  wrapper.addEventListener("touchend", (event) => {
+    const endPosition = event.changedTouches[0].clientX;
+    const { startPosition } = swipePositions;
+    const difference = startPosition - endPosition;
+    if (Math.abs(difference) < 50) {
+      return;
+    }
+
+    difference > 0
+      ? calculateAscendingDirection()
+      : calculateDescendingDirection();
+  });
+
+  function triggerAutoSlide() {
+    intervalID = setInterval(() => {
+      isStart = isStart ?? true;
+      if (autoSlide && !loop) {
+        isNoLoopSliderTurnEnd();
+        return;
+      } else if (autoSlide && loop) {
+        isStart
+          ? calculateAscendingDirection()
+          : calculateDescendingDirection();
+      }
+    }, timeout ?? 5000);
+  }
+
+  function isNoLoopSliderTurnEnd() {
+    return isEnd 
+        ? clearInterval(intervalID)
+        : calculateAscendingDirection();
+  }
+
+  autoSlide && triggerAutoSlide();
+}
+
+slider("slider", {
+  wrapperSelector: ".slider-wrapper",
+  itemSelector: ".slider-item",
+  prevArrow: "prev",
+  nextArrow: "next",
+  slidesPerSlide: 1,
+  slidesPerPage: 2,
+  autoSlide: false,
+  timeout: 8000,
+  loop: false,
 });
